3236d27b7d8db2922cef5124fdef8026ba60a536
aldreichh <johnchierdla@gmail.com>
2024-09-09 08:20:54 +0800

    added 2 more vid proj

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -151,6 +151,16 @@ function Projects() {
                     <ReactPlayer url='https://www.youtube.com/watch?v=7ui6ygKyocM' width="100%" height="100%" controls/>
                 </Box>
             </div>
+            <div className="p-2 flex justify-center items-center" data-aos="fade-up"> 
+                <Box sx={{ boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)', p: 0, width: '600px', height: '400px' }}>
+                    <ReactPlayer url='https://www.youtube.com/watch?v=9ZYFo1ycE4s' width="100%" height="100%" controls/>
+                </Box>
+            </div>
+            <div className="p-2 flex justify-center items-center" data-aos="fade-up">
+                <Box sx={{ boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)', p: 0, width: '600px', height: '400px' }}>
+                    <ReactPlayer url='https://www.youtube.com/watch?v=0Ev6Dh4XaKc' width="100%" height="100%" controls/>
+                </Box>
+            </div>
         </div>
 
         <div className="justify-center items-center flex p-10 mt-24 text-center">
